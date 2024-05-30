63789c1cc480a2cae2f2d11f62feefd3e67d91e6
imjustinedonn <128692539+imjustinedonn@users.noreply.github.com>
2024-05-31 04:04:11 +0800

    AssessmentModule3.sol

---
+++ AssessmentModule3.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract JustineToken is ERC20, Ownable {
+    constructor() ERC20("JustineToken", "JTN") Ownable(msg.sender) {
+
+        _mint(msg.sender, 1000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    function burnFrom(address from, uint256 amount) public {
+        uint256 currentAllowance = allowance(from, msg.sender);
+        require(currentAllowance >= amount, "ERC20: burn amount exceeds allowance");
+        _approve(from, msg.sender, currentAllowance - amount);
+        _burn(from, amount);
+    }
+}
